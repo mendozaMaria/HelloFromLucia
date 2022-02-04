9c9ba4b9a600ebc08ab4a09b3a8740fc659f487c
marialm3 <luciamendozazumaran@gmail.com>
2022-02-04 16:07:21 -0500

    Adding main file

---
+++ src/mendoza/Main.java
@@ -0,0 +1,13 @@
+package mendoza;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Lucia commit
+        String luciaName = "Lucia";
+        System.out.printf("Hello, my name is %s.%n", luciaName);
+
+    //You can add your name below
+
+    }
+}
